4426ac57a0c1c3c35fc1669563d28ad30e64d0e2
tius1234 <tius1234@nate.com>
2018-03-31 00:03:31 +0900

    기본 설정에 C 언어 추가.

--- baekjoon/1463.py
+++
@@ -1,5 +0,0 @@
-# https://www.acmicpc.net/problem/1463
-
-if __name__ == "__main__":
-
-    print("hello")
---
+++ baekjoon/c/1463.c
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/1463
+
+#include <stdio.h>
+
+int main() {
+
+    printf("hello world\n");
+    
+}
